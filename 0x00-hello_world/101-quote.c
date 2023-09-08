3b91ada9f393eec82dc3be2ccd54e23c126d2a24
FredKurt <fjemedy@gmail.com>
2023-09-08 06:47:05 -0700

    Update the file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #!/bin/bash
 int main(void) 
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, 59);
+    fprintf(stderr, "and that piece of art is useful\" - \
+    Dora Korpar, 2015-10-19\n");
     return 1;
 }
