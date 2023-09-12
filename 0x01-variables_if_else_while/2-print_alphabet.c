64ab4d0235c3c7a445f32d8c0ac7226dae966f0d
FREDERIC JEMEDY NGONGANG <102849588+FredKurt@users.noreply.github.com>
2023-09-12 02:51:29 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-    char alphabet;
-
-    for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
     { 
       putchar(alphabet);
-
     }
 
- putchar('\n');
- return (0);
+putchar('\n');
+return (0);
 }
