651df7ec4198643e823902c56717e7f5e0105961
FREDERIC JEMEDY NGONGANG <102849588+FredKurt@users.noreply.github.com>
2023-09-12 02:25:52 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ x = rand()  - RAND_MAX / 2;
 printf("%d \n", x);
 if (x > 0)
 {
-      printf("The number is positive \n");
+printf("The number is positive \n");
 }
 else if (x < 0)
 {
-      printf("The number is negative \n");
+printf("The number is negative \n");
 }
 else
 {
-      printf("The number is zero \n");
+printf("The number is zero \n");
 }
 return (0);
 }
